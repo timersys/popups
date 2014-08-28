df3e7318e2cbd66420edcf8f7008449cbce5be02
timersys <info@timersys>
2014-08-28 10:12:58 -0300

    fixed constants definition

--- popups.php
+++ popups.php
@@ -30,9 +30,9 @@ if ( ! defined( 'WPINC' ) ) {
  * Public-Facing Functionality
  *----------------------------------------------------------------------------*/
 
-define( SPU_PLUGIN_DIR , plugin_dir_path(__FILE__) );
-define( SPU_PLUGIN_URL , plugin_dir_url(__FILE__) );
-define( SPU_PLUGIN_HOOK , basename( dirname( __FILE__ ) ) . '/' . basename( __FILE__ ) );
+define( 'SPU_PLUGIN_DIR' , plugin_dir_path(__FILE__) );
+define( 'SPU_PLUGIN_URL' , plugin_dir_url(__FILE__) );
+define( 'SPU_PLUGIN_HOOK' , basename( dirname( __FILE__ ) ) . '/' . basename( __FILE__ ) );
 
 
 require_once( plugin_dir_path( __FILE__ ) . 'public/class-social-popup.php' );
