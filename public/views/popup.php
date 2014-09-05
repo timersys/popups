659860fa2eaf4a6cd55cc5cecec9ac954027eb64
timersys <info@timersys>
2014-09-05 14:04:43 -0300

    added opts to atts filter

--- public/views/popup.php
+++ public/views/popup.php
@@ -44,7 +44,7 @@ do_action( 'spu/popup/before_popup', $box, $opts, $css);
  data-trigger-number="<?php echo esc_attr( absint( $opts['trigger_number'] ) ); ?>" 
  data-animation="<?php echo esc_attr($opts['animation']); ?>" data-cookie="<?php echo esc_attr( absint ( $opts['cookie'] ) ); ?>" data-test-mode="<?php echo esc_attr($opts['test_mode']); ?>" 
  data-auto-hide="<?php echo esc_attr($opts['auto_hide']); ?>" data-bgopa="<?php echo esc_attr($css['bgopacity']);?>" data-advanced-close="1" data-total="<?php echo $total_shortcodes[$box->ID];?>"
- style="left:-99999px" <?php echo apply_filters( 'spu/popup/data_attrs', $data_attrs );?>>
+ style="left:-99999px" <?php echo apply_filters( 'spu/popup/data_attrs', $data_attrs, $opts);?>>
 	<div class="spu-content"><?php echo $content; ?></div>
 	<span class="spu-close">&times;</span>
 	<span class="spu-timer"></span>
