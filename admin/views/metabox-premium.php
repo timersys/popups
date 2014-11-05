e7842feeae9d113f7065f68ccb9fb3adb9d14f50
timersys <info@timersys>
2014-11-05 10:35:12 -0300

    premium specs

--- admin/views/metabox-premium.php
+++ admin/views/metabox-premium.php
@@ -5,6 +5,9 @@ if ( !defined( 'ABSPATH' ) ) exit;
 <p><?php _e( 'Take the best WordPress Popups plugin to the next level with Popups Premium extension.', $this->plugin_slug );?></p>
 <h2><?php _e( 'Popups Premium Features:', $this->plugin_slug );?></h2>
 <ul>
+	<li><?php _e( 'Track impressions and Conversions of social likes/follow and forms submissions like Contact Form 7, Gravity forms, etc', $this->plugin_slug );?></li>
+	<li><?php _e( 'Track impressions and Conversions also in Google Analytics', $this->plugin_slug );?></li>
+	<li><?php _e( 'Exit Intent technology', $this->plugin_slug );?></li>
 	<li><?php _e( '8 New animations effects', $this->plugin_slug );?> - <a href="http://wp.timersys.com/popups/?utm_source=Plugin&utm_medium=demo-button&utm_campaign=Popups%20Premium">Online demo</a></li>
 	<li><?php _e( 'Exit Intent technology', $this->plugin_slug );?></li>
 	<li><?php _e( 'New trigger methods', $this->plugin_slug );?></li>
@@ -13,7 +16,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 	<li><?php _e( 'Ability to disable Advanced close methods like esc or clicking outside of the popup', $this->plugin_slug );?></li>
 	<li><?php _e( 'Premium support', $this->plugin_slug );?></li>
 </ul>
-<p><strong>Hurry up and get your copy!</strong> Take advantage of this <span style="color:red">launch offer</span> before the price goes up. We have a <strong>lot of new features</strong> to be added soon like mailchimp integration, Google analytics, tracking, etc</p>
+<p><strong>Hurry up and get your copy!</strong> Take advantage of this <span style="color:red">launch offer</span> before the price goes up. We have a <strong>lot of new features</strong> to be added soon like mailchimp integration, aweber, etc</p>
 <p style="text-align:center">
 	<a class="button-primary" href="http://wp.timersys.com/downloads/popups-premium/?utm_source=Plugin&utm_medium=buy-button&utm_campaign=Popups%20Premium"><?php _e( 'Buy Now!', $this->plugin_slug );?></a>
 </p>
