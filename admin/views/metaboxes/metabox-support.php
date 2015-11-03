74fcfda358f4f851c7ef24ad5e3b1a26bb3942a3
Damian Logghe <damian@timersys.com>
2015-11-02 21:56:21 -0300

    fixed support link

--- admin/views/metaboxes/metabox-support.php
+++ admin/views/metaboxes/metabox-support.php
@@ -4,4 +4,4 @@ if ( !defined( 'ABSPATH' ) ) exit;
 ?>
 
 	<p><?php echo sprintf( __( 'You are using Popups plugin v%s', 'popups' ), SocialPopup::VERSION );?></p>
-	<p><?php echo sprintf( __( 'If you need support please go to the WordPress.org <a href="%s">support forums</a>', 'popups' ), 'http://wordpress.org/support/plugin/http://wordpress.org/plugins/popups/');?></p>
+	<p><?php echo sprintf( __( 'If you need support please go to the WordPress.org <a href="%s">support forums</a>', 'popups' ), 'http://wordpress.org/support/plugin/popups/');?></p>
