9e47fd5ddf850a4d5f8afa8b3768213919f3f0d6
Damian Logghe <damian@timersys.com>
2017-04-20 11:26:59 -0300

    added freemius

--- popups.php
+++ popups.php
@@ -25,6 +25,59 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+// Create a helper function for easy SDK access.
+function pop_fs() {
+    global $pop_fs;
+
+    if ( ! isset( $pop_fs ) ) {
+        // Include Freemius SDK.
+        require_once dirname(__FILE__) . '/freemius/start.php';
+
+        $pop_fs = fs_dynamic_init( array(
+            'id'                  => '990',
+            'slug'                => 'popups',
+            'type'                => 'plugin',
+            'public_key'          => 'pk_6e3112a0da9c3f6324200f829bead',
+            'is_premium'          => false,
+            'has_addons'          => false,
+            'has_paid_plans'      => false,
+            'menu'                => array(
+                'slug'           => 'edit.php?post_type=spucpt',
+                'account'        => false,
+				'contact'        => false,
+            ),
+        ) );
+    }
+
+    return $pop_fs;
+}
+
+// Init Freemius.
+pop_fs();
+// Signal that SDK was initiated.
+do_action( 'pop_fs_loaded' );
+
+function pop_fs_custom_connect_message_on_update(
+	$message,
+	$user_first_name,
+	$plugin_title,
+	$user_login,
+	$site_link,
+	$freemius_link
+) {
+	return sprintf(
+		__fs( 'hey-x' ) . '<br>' .
+		__( 'Please help us improve %2$s! If you opt-in, some data about your usage (no private data) of %2$s will be sent to us. If you skip this, that\'s okay! %2$s will still work just fine.', 'popups' ),
+		$user_first_name,
+		'<b>' . $plugin_title . '</b>',
+		'<b>' . $user_login . '</b>',
+		$site_link,
+		$freemius_link
+	);
+}
+
+pop_fs()->add_filter('connect_message_on_update', 'pop_fs_custom_connect_message_on_update', 10, 6);
+
 
 /*----------------------------------------------------------------------------*
  * Public-Facing Functionality
@@ -77,4 +130,24 @@ if ( is_admin() ) {
 	if( defined('WP_CACHE') && !get_option('spu_enabled_cache') )
 			add_action( 'admin_notices', array( $spu_notices, 'enabled_cache') );
 
+}
+// Uninstall now handled on freemius
+pop_fs()->add_action('after_uninstall', 'pop_fs_uninstall_cleanup');
+function pop_fs_uninstall_cleanup(){
+	$opts = get_option( 'spu_settings' );
+
+	if( isset( $opts['uninstall']) && '1' == $opts['uninstall'] ) {
+		// delete settings
+		delete_option('spu_settings');
+		delete_option('spu-version');
+		// delete popups
+		global $wpdb;
+		// IF wpml is active and spucpt is translated get correct ids for language
+		$ids = $wpdb->get_results( "SELECT ID FROM $wpdb->posts WHERE post_type='spucpt'");
+		if( !empty( $ids ) ) {
+			foreach( $ids as $p ) {
+				wp_delete_post( $p->ID, true);
+			}
+		}
+	}
 }
